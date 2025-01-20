4ef1ab135eef09cace2103b7202a1da72a9c6f97
chriscauley <379151+chriscauley@users.noreply.github.com>
2025-01-20 14:16:49 -0500

    adding tailwind and @unrest/tailwind

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
-import './css/index.css'
-import App from './App.vue'
 
+import './css/index.css'
+import '@unrest/tailwind/dist.css'
+import App from './App.vue'
 import store from '@/store'
+
 
 const app = createApp(App)
   .use(store)
